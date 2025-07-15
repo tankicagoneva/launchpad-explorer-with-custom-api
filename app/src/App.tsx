e8edc9fdd25b1cca0c10b6b548b682efe59f2a69
Your <your_email@example.com>
2025-07-15 21:12:47 +0100

    feat: enhance user authentication and role-based routing in the application

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,30 @@
 import Launchpads from "./launchpads/page";
-import logo from "./assets/logo.svg";
 import { ThemeProvider } from "./components/ui/theme-provider";
 import { ToastContainer } from "react-toastify";
+import { useUser } from '@clerk/clerk-react';
+import { Navigate } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const { user, isLoaded } = useUser();
+  const userRole = user?.publicMetadata?.role as string;
+
+  if (isLoaded && user) {
+    if (userRole === 'astronaut') {
+      return <Navigate to="/astronaut-dashboard" replace />;
+    }
+    if (userRole === 'admin') {
+      return <Navigate to="/dashboard" replace />;
+    }
+  }
+
   return (
     <>
       <ThemeProvider defaultTheme="system" storageKey="ui-theme">
         <div className="space-y-8 p-8">
-          <img src={logo} alt="Launchpad explorer logo" width="300px;" />
-          <Launchpads />
+          
+          {(userRole !== 'astronaut') && <Launchpads />}
+      
           <ToastContainer />
         </div>
       </ThemeProvider>
